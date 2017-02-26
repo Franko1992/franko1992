9015ff48aeb7d0c5ef4d83b27e580459587a8aa4
1-Kompuiter\1 <kingaanowak1995@gmail.com>
2017-02-26 23:27:56 +0100

    ...

--- javascript.js
+++ javascript.js
@@ -32,5 +32,21 @@ $(document).ready(function() {
 
 
 
+				$(document).ready(function(){
+
+				  $('body').scrollspy({target: ".navbar", offset: 50});
+				  $("#navbar a").on('click', function(event) {
+				    if (this.hash !== "") {
+				      event.preventDefault();
+				      var hash = this.hash;
+				      $('html, body').animate({
+				        scrollTop: $(hash).offset().top
+				      }, 1000, function(){
+				        window.location.hash = hash;
+				      });
+				    }
+				  });
+				});
+
 
 });
